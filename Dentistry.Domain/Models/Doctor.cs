eb34b4dfe4edac90d59ec168549e96575359ba9a
LXTsinenergy <bellerstas3002@gmail.com>
2023-06-28 21:38:38 +0300

    Создание моделей врача и достижения, перечисления специальностей

---
+++ Dentistry.Domain/Models/Doctor.cs
@@ -0,0 +1,34 @@
+﻿using Dentistry.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dentistry.Domain.Models
+{
+    public class Doctor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public int Experience { get; set; }
+
+        [Required]
+        public List<Specialties> Specialties { get; set; }
+
+        public List<Achievement> Achievements { get; set; }
+
+        [Required]
+        public List<string> Certificates { get; set; }
+
+        [Required]
+        public List<string> Education { get; set; }
+
+        [Required]
+        public List<string> PlacesOfWork { get; set; }
+
+        [Required]
+        public List<string> Reviews { get; set; }
+    }
+}
