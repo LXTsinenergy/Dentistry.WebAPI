98e7e182ce2b8c216ba3648ce64b5ed6b49d250a
LXTsinenergy <bellerstas3002@gmail.com>
2023-06-28 21:06:51 +0300

    Создание базы данных (code-first)

---
+++ Dentistry.DAL/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Dentistry.DAL.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+    }
+}
