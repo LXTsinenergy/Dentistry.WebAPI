36ccb47e840d6557a69ac2f6be0ff4d38c8caa5a
LXTsinenergy <bellerstas3002@gmail.com>
2023-06-29 15:36:13 +0300

    Создание сервиса пользователя

---
+++ Dentistry.BLL/Services/UserService/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using Dentistry.Domain.DTO;
+
+namespace Dentistry.BLL.Services.UserService
+{
+    public interface IUserService
+    {
+        Task Add(UserDTO userDTO);
+    }
+}
---
+++ Dentistry.BLL/Services/UserService/UserService.cs
@@ -0,0 +1,12 @@
+﻿using Dentistry.Domain.DTO;
+
+namespace Dentistry.BLL.Services.UserService
+{
+    public class UserService : IUserService
+    {
+        public Task Add(UserDTO userDTO)
+        {
+            
+        }
+    }
+}
