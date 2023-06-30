170f1eeadc40434db6709168b538a39efdf69459
LXTsinenergy <bellerstas3002@gmail.com>
2023-06-30 18:34:57 +0300

    Настройки авторизации и аутентификации в Program.cs

--- Dentistry.API/Program.cs
+++ Dentistry.API/Program.cs
@@ -2,7 +2,11 @@ using Dentistry.BLL.Mapping;
 using Dentistry.BLL.Services.UserService;
 using Dentistry.DAL.DataContext;
 using Dentistry.DAL.Repositories.UserRepository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,11 +25,38 @@ builder.Services.AddDbContext<ApplicationDbContext>(
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
 builder.Services.Configure<RouteOptions>(o =>
+{
+    o.LowercaseUrls = true;
+    o.LowercaseQueryStrings = true;
+    o.AppendTrailingSlash = true;
+});
+
+builder.Services.AddSwaggerGen(swagger =>
+{
+    swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
     {
-        o.LowercaseUrls = true;
-        o.LowercaseQueryStrings = true;
-        o.AppendTrailingSlash = true;
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization"
     });
+    swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 #endregion
 
@@ -40,6 +71,30 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 #region Services
 
 builder.Services.AddScoped<IUserService, UserService>();
+
+#endregion
+
+
+#region Authorization and Authentication
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(option =>
+    {
+        option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = false,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "Jwt:Issuer",
+            ValidAudience = "Jwt:Audience",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("test"))
+        };
+    });
 
 #endregion
 
