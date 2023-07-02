334464e0531ef3131e5f41f5cae1304110afe769
LXTsinenergy <bellerstas3002@gmail.com>
2023-07-02 19:02:42 +0300

    Аттрибуты в UserDTO

--- Dentistry.Domain/DTO/UserDTO.cs
+++ Dentistry.Domain/DTO/UserDTO.cs
@@ -1,13 +1,27 @@
 ﻿using Dentistry.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace Dentistry.Domain.DTO
 {
     public class UserDTO
     {
+        [Required]
         public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [RegularExpression("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", ErrorMessage = "Введите действительный Email-адрес")]
         public string Email { get; set; }
+
+        [Required]
         public string Password { get; set; }
+
+        [Required]
+        [Phone]
+        [RegularExpression("^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$", ErrorMessage = "Введите действительный номер телефона")]
         public string PhoneNumber { get; set; }
+
+        [Required]
         public Role Role { get; set; }
     }
 }
