9711c61145f68c90507b8f2e90488e005740af08
LXTsinenergy <bellerstas3002@gmail.com>
2023-07-02 15:40:31 +0300

    Добавление методов генерации соли и хэширования пароля

---
+++ Dentistry.BLL/Services/PasswordService/IPasswordService.cs
@@ -0,0 +1,7 @@
+﻿namespace Dentistry.BLL.Services.PasswordService
+{
+    public interface IPasswordService
+    {
+        byte[] HashPassword(string password, byte[] salt);
+    }
+}
---
+++ Dentistry.BLL/Services/PasswordService/PasswordService.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Dentistry.BLL.Services.PasswordService
+{
+    public class PasswordService : IPasswordService
+    {
+        public byte[] HashPassword(string password, byte[] salt)
+        {
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            byte[] saltedPassword = new byte[salt.Length + passwordBytes.Length];
+
+            using var sha256 = SHA256.Create();
+            return sha256.ComputeHash(saltedPassword);
+        }
+
+        public byte[] GenerateSalt()
+        {
+            const int saltLength = 64;
+            byte[] salt = new byte[saltLength];
+
+            var randomNumberGeneratot = RandomNumberGenerator.Create();
+            randomNumberGeneratot.GetBytes(salt);
+
+            return salt;
+        }
+    }
+}
